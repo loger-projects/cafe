d3d117f9cb366aec79f0556306dc33411f596916
logernam <logernam@gmail.com>
2019-02-25 20:54:17 +0700

    hieu duoc cach selector trong vuejs _ note: on lai kien thuc ve javascript _ that ky _ dung co lam lung tung

--- resources/src/components/SitePageHomeHeaderSlider.vue
+++ resources/src/components/SitePageHomeHeaderSlider.vue
@@ -1,10 +1,34 @@
 <template>
     <div id="site-page-home-header-slider">
-        ul
+        <ul class="background-slider">
+            <li class="bg-slide-item"></li>
+            <li class="bg-slide-item"></li>
+            <li class="bg-slide-item"></li>
+            <li class="bg-slide-item"></li>
+            <li class="bg-slide-item"></li>
+        </ul>
     </div>
 </template>
 <style lang="scss" scoped>
-    
+    div#site-page-home-header-slider {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: -1;
+    }
+    ul.background-slider {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        li.bg-slide-item {
+            position: relative;
+            width: 100%;
+            height: 100%;
+            opacity: 0;
+        }
+    }
 </style>
 <script>
 export default {
